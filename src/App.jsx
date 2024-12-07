bf5b7529bb863de695d484221034fcb196a069f0
aaron_tetracube <aaronpsam@gmail.com>
2024-12-07 18:48:27 +0530

    added random number generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] =useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenarator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str+="0123456789";
+    if(charAllowed) str+="!@#$%^&*()_+-=[]{}|;:<>?/~`"
+
+    for(let i =1;i <= array.length; i++){
+      let char = Math.floor(Math.random * str.length + 1)
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+
+  },[length, numberAllowed,charAllowed,setPassword]);
 
   return (
     <>
